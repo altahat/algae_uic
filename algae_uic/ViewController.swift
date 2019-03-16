483039389e6b9c5ec42e756d732151fd617482c2
Zaid Altahat <21979030+altahat@users.noreply.github.com>
2019-03-16 14:38:38 -0500

    Initial Commit

---
+++ algae_uic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  algae_uic
+//
+//  Created by zaid on 3/16/19.
+//  Copyright © 2019 zaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
