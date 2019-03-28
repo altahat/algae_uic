5102fcaafe10b3408d28cee9beff9fe89d638579
dtruon4 <dtruon4@uic.edu>
2019-03-28 18:22:43 -0500

    Added algae_0_1_1_1 files
    -added view to storyboard
    -added algae_0_1_1_1_ViewController.swift
    -added algae_0_1_1_1_Delegate.swift

--- algae_uic/algae_0_1_1_1_ViewController.swift
+++ algae_uic/algae_0_1_1_1_ViewController.swift
@@ -1,15 +1,14 @@
 //
-//  algae_0_1_1_ViewController.swift
+//  algae_0_1_1_1_ViewController.swift
 //  algae_uic
 //
 //  Created by Darmawan Truong on 3/28/19.
 //  Copyright © 2019 zaid. All rights reserved.
 //
 
-
 import UIKit
 
-class algae_0_1_1_ViewController: UIViewController {
+class algae_0_1_1_1_ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
