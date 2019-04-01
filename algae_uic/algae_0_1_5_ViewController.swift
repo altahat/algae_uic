58429bce5e445b24b5be445425831952f06d5bac
twebb <thomaswebb1991@gmail.com>
2019-04-01 15:49:30 -0500

    About Us Section
    Created About Us Section

---
+++ algae_uic/algae_0_1_5_ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  algae_0_1_5_ViewController.swift
+//  algae_uic
+//
+//  Created by Student on 4/1/19.
+//  Copyright © 2019 zaid. All rights reserved.
+//
+
+import UIKit
+
+class algae_0_1_5_ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
