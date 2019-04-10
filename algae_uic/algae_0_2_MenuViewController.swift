b36b3aa25f371b4c916925dde36581108b24beda
Gabriel <gabriel_mui@hotmail.com>
2019-04-10 03:06:36 -0500

    algae_0_2
    done

--- algae_uic/algae_0_2_MenuViewController.swift
+++ algae_uic/algae_0_2_MenuViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
+enum MenuType: Int {
+    case VisualEstimate
+    case ViewInputData
+    case AboutAlgae
+    case Map
+    case About
+}
+
+
 class algae_0_2_MenuViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+       tableView.backgroundView = UIImageView(image: UIImage(named: "bg"))
     }
     
-    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let menuType = MenuType(rawValue: indexPath.row) else { return }
+        dismiss(animated: true) { [weak self] in
+            print("Dismissing: \(menuType)")
+            //self?.didTapMenuType?(menuType)
+        }
+    }
 }
