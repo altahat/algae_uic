4751b43a347dae39ac8f5221f216b64d9a16a3e9
Gabriel <gabriel_mui@hotmail.com>
2019-04-10 01:54:41 -0500

    algae_0_2 upload
    slide out menu logo not showing

---
+++ algae_uic/algae_0_2_MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  algae_0_2_MenuViewController.swift
+//  algae_uic
+//
+//  Created by GabrielMui on 4/10/19.
+//  Copyright © 2019 zaid. All rights reserved.
+//
+
+import UIKit
+
+class algae_0_2_MenuViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
---
+++ algae_uic/algae_0_2_ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  algae-0_2_ViewController.swift
+//  algae_uic
+//
+//  Created by GabrielMui on 4/10/19.
+//  Copyright © 2019 zaid. All rights reserved.
+//
+
+import UIKit
+
+class algae_0_2_ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
+
